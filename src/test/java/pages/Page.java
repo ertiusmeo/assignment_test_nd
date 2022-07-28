1b98f057ccdd880b0bb9b1f77ae524e64aff1f91
Darek <Darek@DESKTOP-05L56RN>
2022-07-28 20:16:04 +0200

    refactor

--- src/test/java/pages/Page.java
+++ src/test/java/pages/Page.java
@@ -6,16 +6,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class Page {
 
-    protected WebDriver driver;
-    public WebDriverWait wait;
+    WebDriver driver;
+    WebDriverWait wait;
 
     public Page(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     By breadcrumb = By.xpath("//*[contains(@class, 'breadcrumb row')and contains(@class, 'medium')]");
